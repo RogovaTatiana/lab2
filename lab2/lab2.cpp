d8314e76962ae2765e73e0fa88372cf386f38109
RogovaTatyana <marketer@bk.ru>
2015-05-19 01:03:10 +0300

    open mistake

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -52,14 +52,18 @@ void main()
 
 	ifstream fin;
 	fin.open("D:\\assortment.txt", ios::in);
-	string str;
-	getline(fin, str);   //записываем в str первую строку.
-	while (!fin.eof())    //пока не конец файла, мы исполняем цикл.
+	if (fin.is_open())
 	{
-		cout << str << "\n";        //выводим на экран первую строку.
-		getline(fin, str); //записываем вторую.
+		string str;
+		getline(fin, str);   //записываем в str первую строку.
+		while (!fin.eof())    //пока не конец файла, мы исполняем цикл.
+		{
+			cout << str << "\n";        //выводим на экран первую строку.
+			getline(fin, str); //записываем вторую.
+		}
+		fin.close();
 	}
-	fin.close();
+	else cout << "Ошибка при открытии файла";
 	system("pause");
 
 	// Поиск записей
@@ -70,71 +74,75 @@ void main()
 	// 4. вывод на экран
 
 	fin.open("D:\\assortment.txt", ios::in);
-	Toy *newToys[30];
-	int m = 0;
-	string str2;
-	getline(fin, str2);
-	while (!fin.eof())
+	if (fin.is_open())
 	{
-		string word[5];
-		int pos = 0;
-		for (int i = 0; i < 5; i++)
-		{
-			int t_pos = str2.find("\t", pos);
-			word[i] = str2.substr(pos, (t_pos - pos));
-			pos = t_pos + 1;
-		}
-		newToys[m] = new Toy();
-		strcpy(newToys[m]->name, word[0].c_str());
-		newToys[m]->price = atof(word[1].c_str());
-		newToys[m]->quant = atoi(word[2].c_str());
-		newToys[m]->ageFrom = atoi(word[3].c_str());
-		newToys[m]->ageTo = atoi(word[4].c_str());
-
+		Toy *newToys[30];
+		int m = 0;
+		string str2;
 		getline(fin, str2);
-		m++;
-	}
-	fin.close();
-	cout << "Игрушки, которые подходят детям от 1 до 3: \n";
-	for (int i = 0; i < m; i++)
-	{
-		if (newToys[i]->ageFrom <= 1 && newToys[i]->ageTo >= 3)
-			cout << newToys[i]->name << " \n";
-	}
-	system("pause");
-	cout << "Самая дорогая игрушка в ассортименте: \n";
-	double max = newToys[0]->price;
-	int index = 0;
-	for (int i = 1; i < m; i++)
-	{
-		if (newToys[i]->price > max)
+		while (!fin.eof())
 		{
-			max = newToys[i]->price;
-			index = i;
+			string word[5];
+			int pos = 0;
+			for (int i = 0; i < 5; i++)
+			{
+				int t_pos = str2.find("\t", pos);
+				word[i] = str2.substr(pos, (t_pos - pos));
+				pos = t_pos + 1;
+			}
+			newToys[m] = new Toy();
+			strcpy(newToys[m]->name, word[0].c_str());
+			newToys[m]->price = atof(word[1].c_str());
+			newToys[m]->quant = atoi(word[2].c_str());
+			newToys[m]->ageFrom = atoi(word[3].c_str());
+			newToys[m]->ageTo = atoi(word[4].c_str());
+
+			getline(fin, str2);
+			m++;
+		}
+		fin.close();
+		cout << "Игрушки, которые подходят детям от 1 до 3: \n";
+		for (int i = 0; i < m; i++)
+		{
+			if (newToys[i]->ageFrom <= 1 && newToys[i]->ageTo >= 3)
+				cout << newToys[i]->name << " \n";
+		}
+		system("pause");
+		cout << "Самая дорогая игрушка в ассортименте: \n";
+		double max = newToys[0]->price;
+		int index = 0;
+		for (int i = 1; i < m; i++)
+		{
+			if (newToys[i]->price > max)
+			{
+				max = newToys[i]->price;
+				index = i;
+			}
+		}
+		cout << newToys[index]->name << "\t" << newToys[index]->price << " \n";
+		system("pause");
+
+		double price;
+		int ageFrom;
+		int ageTo;
+
+		cout << "Введите условия поиска: \n";
+		cout << "Цена до ";
+		cin >> price;
+		cout << "Возраст от ";
+		cin >> ageFrom;
+		cout << "Возраст до ";
+		cin >> ageTo;
+
+		for (int i = 0; i < m; i++)
+		{
+			if (newToys[i]->ageFrom <= ageFrom && newToys[i]->ageTo >= ageTo && newToys[i]->price <= price)
+				cout << newToys[i]->name << " \n";
 		}
 	}
-	cout << newToys[index]->name << "\t" << newToys[index]->price << " \n";
-	system("pause");
-	
-	double price;
-	int ageFrom;
-	int ageTo;
+	else cout << "Ошибка при открытии файла";
 
-	cout << "Введите условия поиска: \n";
-	cout << "Цена до ";
-	cin >> price;
-	cout << "Возраст от ";
-	cin >> ageFrom;
-	cout << "Возраст до ";
-	cin >> ageTo;
-	
-	for (int i = 0; i < m; i++)
-	{
-		if (newToys[i]->ageFrom <= ageFrom && newToys[i]->ageTo >= ageTo && newToys[i]->price <= price)
-			cout << newToys[i]->name << " \n";
-	}
 	system("pause");
-
 	return;
 }
 
